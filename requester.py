87ba09b5b3d9aa478d27573b4cd6ee8883f55945
Ladislav Burgr <burgr.ladislav@gmail.com>
2024-01-27 14:46:49 +0100

    Improve memory efficiency, fix bugs, ready for the first attempt to gather all data

--- requester.py
+++ requester.py
@@ -1,34 +1,31 @@
 import requests
 import xml.etree.ElementTree as ET
 import csv
+from time import sleep
+
+# TODO: F06?
 
 # how many submissions to get
-NO_OF_SUBMISSIONS = 500
-
-# defining the api-endpoint
-# API_ENDPOINT = f"https://api.vvz.nipez.cz/api/submissions/search?page=1&limit={NO_OF_SUBMISSIONS}&form=vz&workflowPlace=UVEREJNENO_VVZ&data.druhFormulare=F03&order%5Bdata.datumUverejneniVvz%5D=DESC"
+NO_OF_SUBMISSIONS = 100000
 
 # the prefix is necessary (in this format) for XML search (find function) to work
 def getXMLAttr(attr):
     return "{https://www.vvz.nipez.cz/zvz_xml/schemas/vvz/v1.0.0}" + attr
 
 
-def getSearchResults(page, limit):
+def getSearchResultsUrl(page, limit):
     return f"https://api.vvz.nipez.cz/api/submissions/search?page={page}&limit={limit}&form=vz&workflowPlace=UVEREJNENO_VVZ&data.druhFormulare=F03&order%5Bdata.datumUverejneniVvz%5D=DESC"
 
+# 50ms delay to not to overload the server
+# sleep(0.05)
 
-allSubmissions = []
+currSubmissions = []
 submissionRemaining = NO_OF_SUBMISSIONS
 page = 1
-# get all search results (250 is max per request)
-while submissionRemaining > 0:
-    limit = min(250, submissionRemaining)
-    searchRes = requests.get(url=getSearchResults(page, limit))
-    allSubmissions.extend(searchRes.json())
-    submissionRemaining -= limit
-    page += 1
 
-results = []
+
+# Uncomment result variable to print all the results (in larger quantities = memory issues)
+# results = []
 notCZK = 0
 noEstimate = 0
 xmlError = 0
@@ -36,103 +33,134 @@ parsed = 0
 
 with open('results.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(["název", "CPV", "počet soutěžících",
+    writer.writerow(["název zakázky", "zadavatel", "druh", "CPV", "počet soutěžících",
                     "výsledná cena", "očekávaná cena"])
 
-    # iterate all results
-    for submission in allSubmissions:
-        parsed += 1
-        id = submission['submissionVersion'].split('/')[-1]
-        SUBMISSION_FIRST_URL = f'https://api.vvz.nipez.cz/api/submission_attachments?limit=50&submissionVersion={id}'
+    # get all search results (250 is max per request)
+    while submissionRemaining > 0:
+        limit = min(250, submissionRemaining)
+        searchRes = requests.get(url=getSearchResultsUrl(page, limit)).json()
 
-        firstRes = requests.get(url=SUBMISSION_FIRST_URL)
-        publicId = firstRes.json()[0]["publicId"]
+        # reached the end of the results
+        if 'code' in searchRes and searchRes['code'] == "BAD_REQUEST":
+            print("no more results", searchRes)
+            break
 
-        SUBMISSION_SECOND_URL = f'https://api.vvz.nipez.cz/download/submission_attachments/public/{publicId}'
-        secondRes = requests.get(url=SUBMISSION_SECOND_URL)
+        currSubmissions = searchRes
+        submissionRemaining -= limit
+        page += 1
 
-        xmlRoot = ET.fromstring(secondRes.content)
+        # iterate all results
+        for submission in currSubmissions:
+            parsed += 1
+            id = submission['submissionVersion'].split('/')[-1]
+            SUBMISSION_FIRST_URL = f'https://api.vvz.nipez.cz/api/submission_attachments?limit=50&submissionVersion={id}'
 
-        # TODO Add delay to not overload the server
+            firstRes = requests.get(url=SUBMISSION_FIRST_URL)
+            publicId = firstRes.json()[0]["publicId"]
 
-        try:
-            try:
-                title = xmlRoot[0].find(getXMLAttr("OBJECT_CONTRACT")).find(
-                    getXMLAttr("TITLE")).find(getXMLAttr("P")).text
-            except:
-                raise Exception("no title")
+            SUBMISSION_SECOND_URL = f'https://api.vvz.nipez.cz/download/submission_attachments/public/{publicId}'
+            secondRes = requests.get(url=SUBMISSION_SECOND_URL)
 
-            try:
-                cpv = xmlRoot[0].find(getXMLAttr("OBJECT_CONTRACT")).find(
-                    getXMLAttr("CPV_MAIN")).find(getXMLAttr("CPV_CODE")).attrib["CODE"]
-            except:
-                raise Exception("no cpv")
-
-            try:
-                tenderResults = xmlRoot[0].find(getXMLAttr("AWARD_CONTRACT")).find(
-                    getXMLAttr("AWARDED_CONTRACT"))
-            except:
-                raise Exception("no tender results")
+            xmlRoot = ET.fromstring(secondRes.content)
 
             try:
-                values = tenderResults.find(getXMLAttr("VALUES"))
-            except:
-                raise Exception("no values")
+                # název zakázky
+                try:
+                    title = xmlRoot[0].find(getXMLAttr("OBJECT_CONTRACT")).find(
+                        getXMLAttr("TITLE")).find(getXMLAttr("P")).text
+                except:
+                    raise Exception("no title")
 
-            try:
-                estimatedVal = values.find(getXMLAttr("VAL_ESTIMATED_TOTAL"))
+                # zadavatel
+                try:
+                    contractOwner = xmlRoot[0].find(getXMLAttr("CONTRACTING_BODY")).find(
+                        getXMLAttr("ADDRESS_CONTRACTING_BODY")).find(getXMLAttr("OFFICIALNAME")).text
+                except:
+                    raise Exception("no contract owner")
+
+                # druh zakázky
+                try:
+                    kind = ""
+                    xmlKind = xmlRoot[0].find(getXMLAttr("OBJECT_CONTRACT")).find(
+                        getXMLAttr("TYPE_CONTRACT")).attrib["CTYPE"]
+                    if xmlKind == "SUPPLIES":
+                        kind = "Dodávky"
+                    elif xmlKind == "SERVICES":
+                        kind = "Služby"
+                    elif xmlKind == "WORKS":
+                        kind = "Stavební práce"
+                    else:
+                        raise Exception()
+                except:
+                    raise Exception("no kind")
+
+                # CPV
+                try:
+                    cpv = xmlRoot[0].find(getXMLAttr("OBJECT_CONTRACT")).find(
+                        getXMLAttr("CPV_MAIN")).find(getXMLAttr("CPV_CODE")).attrib["CODE"]
+                except:
+                    raise Exception("no cpv")
+
+                # výsledek soutěže
+                try:
+                    tenderResults = xmlRoot[0].find(getXMLAttr("AWARD_CONTRACT")).find(
+                        getXMLAttr("AWARDED_CONTRACT"))
+                except:
+                    raise Exception("no tender results")
+                
+                # počet soutěžících
+                noOfCompetitors = tenderResults.find(getXMLAttr("TENDERS")).find(
+                    getXMLAttr("NB_TENDERS_RECEIVED")).text
+
+                # ceny
+                try:
+                    values = tenderResults.find(getXMLAttr("VALUES"))
+                except:
+                    raise Exception("no values")
+
+                # očekávaná cena
+                try:
+                    estimatedVal = values.find(getXMLAttr("VAL_ESTIMATED_TOTAL"))
+                    if estimatedVal is None:
+                        raise Exception()
+                except:
+                    noEstimate += 1
+                    raise Exception("no estimated value")
+
+                # výsledná cena
+                try:
+                    realVal = values.find(getXMLAttr("VAL_TOTAL"))
+                    if realVal is None:
+                        raise Exception()
+                except:
+                    raise Exception("no real value")
+
                 if estimatedVal is None:
-                    raise Exception()
-            except:
-                raise Exception("no estimated value")
+                    noEstimate += 1
+                    raise Exception("no estimated value")
 
-            try:
-                realVal = values.find(getXMLAttr("VAL_TOTAL"))
-                if realVal is None:
-                    raise Exception()
-            except:
-                raise Exception("no real value")
+                try:
+                    realCurr = realVal.attrib["CURRENCY"]
+                    estimatedCurr = estimatedVal.attrib["CURRENCY"]
+                except:
+                    raise Exception("no currency attribute")
 
-            if estimatedVal is None:
-                print("NO ESTIMATED VALUE")
-                noEstimate += 1
-                continue
+                if (realCurr != "CZK" or estimatedCurr != "CZK"):
+                    notCZK += 1
+                    raise Exception("currency error")
 
-            try:
-                realCurr = realVal.attrib["CURRENCY"]
-                estimatedCurr = estimatedVal.attrib["CURRENCY"]
-            except:
-                raise Exception("no currency attribute")
-
-            if (realCurr != "CZK" or estimatedCurr != "CZK"):
-                notCZK += 1
-                raise Exception("currency error")
-
-            noOfCompetitors = tenderResults.find(getXMLAttr("TENDERS")).find(
-                getXMLAttr("NB_TENDERS_RECEIVED")).text
-
-            results.append(
-                {"title": title, "cpv": cpv, "real": realVal.text, "estimated": estimatedVal.text, "competitors": noOfCompetitors})
-            writer.writerow([title, cpv, noOfCompetitors,
-                            realVal.text, estimatedVal.text])
-        except Exception as err:
-            xmlError += 1
-            print("ERROR IN XML", err, '(', SUBMISSION_SECOND_URL, ')')
+                # results.append(
+                #     {"title": title, "contractOwner": contractOwner, "kind": kind, "cpv": cpv, "real": realVal.text, "estimated": estimatedVal.text, "competitors": noOfCompetitors})
+                writer.writerow([title, contractOwner, kind, cpv, noOfCompetitors,
+                                realVal.text, estimatedVal.text])
+            except Exception as err:
+                xmlError += 1
+                # print("ERROR IN XML", err, '(', SUBMISSION_SECOND_URL, ')')
 
 # print(results)
 print("celkem zpracováno", parsed)
 print("ne v CZK", notCZK)
 print("bez odhadu ceny", noEstimate)
-print("chyb v parsování XML", xmlError)
-# notes:
-# get all 100 000 ids
-# get param submissionVersion
-# go to its endpoint (i.e. "/api/submission_versions/7bc9c387-a901-4ad7-a0c2-58768758546a") at api https://api.vvz.nipez.cz/api/submission_attachments?limit=50&submissionVersion=7bc9c387-a901-4ad7-a0c2-58768758546a
-# get publicId param and use it at API https://api.vvz.nipez.cz/download/submission_attachments/public/6RYwKzRTMV84Xza7bdo4avFupJhpEdaS
-# compare <VAL_ESTIMATED_TOTAL CURRENCY="CZK">1380000</VAL_ESTIMATED_TOTAL> and <VAL_TOTAL CURRENCY="CZK">1321969</VAL_TOTAL>
+print("celkem chyb v parsování XML (nejčasttěji chybějící cena, nevyhodnocená zakázka atp.)", xmlError)
 
-
-# extracting data in json format
-
-
-# print(data)
